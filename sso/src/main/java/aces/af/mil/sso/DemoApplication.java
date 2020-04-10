ab8bad4844870956817e9ebed32076f7a0b3097e
Kevin Summersill <ksummersill@appddictionstudio.com>
2020-04-10 09:34:14 -0500

    updated rebaseline

--- sso/src/main/java/aces/af/mil/sso/DemoApplication.java
+++ sso/src/main/java/aces/af/mil/sso/DemoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SsoApplication {
+public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SsoApplication.class, args);
+		SpringApplication.run(DemoApplication.class, args);
 	}
 
 }
