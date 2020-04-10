3dcbe139c811519f1dfdd9eee3acc5dd464f623f
Kevin Summersill <ksummersill@appddictionstudio.com>
2020-04-10 16:58:59 -0500

    pushing up the repository

--- backend/src/main/java/aces/af/mil/controller/PortalController.java
+++ backend/src/main/java/aces/af/mil/controller/PortalController.java
@@ -5,8 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/aces-portal/home")
-
+@RequestMapping("/")
 public class PortalController {
     @GetMapping
     public String home() {
--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { HomepageComponent } from './homepage/homepage.component';
 
 const routes: Routes = [
   {
-    path: 'home',
+    path: '',
     component: HomepageComponent
   }
 ];
